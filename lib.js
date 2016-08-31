495e2d6a4f0ed6e222323a32970ba5ccb203e70a
Danny Dulai <danny@ishiboo.com>
2016-08-31 12:19:01 -0400

    initial commit

---
+++ lib.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let SVCNAME = "com.roonlabs.browse:1";
+
+function RoonApiBrowse(core) {
+    this.core = core;
+}
+
+RoonApiBrowse.services = [ { name: SVCNAME } ];
+
+RoonApiBrowse.prototype.begin = function(opts, cb) {
+    this.core.moo.send_request(SVCNAME+"/begin",
+                               opts,
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"), body);
+                               });
+};
+RoonApiBrowse.prototype.browse = function(opts, cb) {
+    this.core.moo.send_request(SVCNAME+"/browse",
+                               opts,
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"), body);
+                               });
+};
+RoonApiBrowse.prototype.load = function(opts, cb) {
+    this.core.moo.send_request(SVCNAME+"/load",
+                               opts,
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"), body);
+                               });
+};
+RoonApiBrowse.prototype.pop_all = function(opts, cb) {
+    this.core.moo.send_request(SVCNAME+"/pop_all",
+                               opts,
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"), body);
+                               });
+RoonApiBrowse.prototype.pop= function(opts, cb) {
+    this.core.moo.send_request(SVCNAME+"/pop",
+                               opts,
+                               (msg, body) => {
+                                   if (cb)
+                                       cb(msg && msg.name == "Success" ? false : (msg ? msg.name : "NetworkError"), body);
+                               });
+};
+};
+
+exports = module.exports = RoonApiBrowse;
